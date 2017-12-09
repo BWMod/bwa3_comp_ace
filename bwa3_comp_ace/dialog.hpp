a846ea4740dbb4fad995921dfc48f9af9e723eb5
Thomas Müller <Dorbedo@t-online.de>
2017-12-09 11:41:44 +0100

    scrollbar

--- bwa3_comp_ace/dialog.hpp
+++ bwa3_comp_ace/dialog.hpp
@@ -70,15 +70,25 @@ class BWA3_MagazineDialog {
             selectedRowColorTo[]    = {0.7, 0.85, 1, 0.5};
             selectedRowAnimLength = 1.2;
 
-            class VScrollBar: ScrollBar {
+            class VScrollBar {
+                color[] = {1,1,1,0.6};
+                colorActive[] = {1,1,1,1};
+                colorDisabled[] = {1,1,1,0.3};
+                thumb = "\A3\ui_f\data\gui\cfg\scrollbar\thumb_ca.paa";
+                arrowEmpty = "\A3\ui_f\data\gui\cfg\scrollbar\arrowEmpty_ca.paa";
+                arrowFull = "\A3\ui_f\data\gui\cfg\scrollbar\arrowFull_ca.paa";
+                border = "\A3\ui_f\data\gui\cfg\scrollbar\border_ca.paa";
+                shadow = 0;
+                scrollSpeed = 0.06;
                 width = GUI_GRID_W * 0.8;
                 autoScrollEnabled = 0;
                 autoScrollDelay = 1;
                 autoScrollRewind = 1;
                 autoScrollSpeed = 1;
             };
-            class HScrollBar: ScrollBar {
+            class HScrollBar: VScrollBar {
                 height = 0;
+                width = 0;
             };
             class HeaderTemplate {
                 class HeaderBackground {
